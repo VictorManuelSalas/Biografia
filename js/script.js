73d7c70e12f7bdf0f4dc09809a8e22c6386735a0
VictorMNSalas <victorsalasnz@gmail.com>
2023-02-14 17:58:59 -0600

    phrases

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+    setTimeout(() => {
+        var load = document.getElementById("loader");
+        load.style.display = "none";
+    }, 1000);
+});
+
+copyright = document.getElementById("copyright");
