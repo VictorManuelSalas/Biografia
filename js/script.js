dd96fd227cb874e0629aa46ad66dc887d0025d58
VictorMNSalas <victorsalasnz@gmail.com>
2023-02-14 23:25:01 -0600

    hader and footer

--- js/script.js
+++ js/script.js
@@ -5,4 +5,17 @@ window.addEventListener("load", function () {
     }, 1000);
 });
 
+
+
+var fecha = new Date();
+var año = fecha.getFullYear();
+
 copyright = document.getElementById("copyright");
+
+var txt = "&#169;" + año + " Charlie Munger";
+copyright.innerHTML = txt;
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY>10);
+})
